0934af2ce7da5adf3b5b5d84e0407adb890efe5b
frankPezo <frankanderpg.2000@gmail.com>
2025-11-30 19:19:42 -0500

    Primer commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+  require 'header/header.php';
+?>
+
+   <div class="container flex item-center justify-center min-h-screen">
+            <div class="">
+
+            </div>
+   </div>
+
+ <?php
+  require 'footer/footer.php';
+?>
